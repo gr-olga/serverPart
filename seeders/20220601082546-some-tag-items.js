e0ee2afaed0ab6f3092c3138c5c52b373cd3ccb0
Olga Panfilova <gr-olga@ya.ru>
2022-06-01 11:33:00 +0200

    many to many in data

--- seeders/20220601082546-some-tag-items.js
+++ seeders/20220601082546-some-tag-items.js
@@ -12,7 +12,7 @@ module.exports = {
                     updatedAt: new Date(),
                 },
                 {
-                    todoItemId: 3,
+                    todoItemId: 1,
                     tagId: 1,
                     createdAt: new Date(),
                     updatedAt: new Date(),
@@ -28,7 +28,6 @@ module.exports = {
         )
     },
 
-    async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("itemTags", null, {});
+    async down(queryInterface, Sequelize) {await queryInterface.bulkDelete("itemTags", null, {});
     }
 };
